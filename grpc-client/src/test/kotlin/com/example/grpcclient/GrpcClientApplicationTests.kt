d73a843dbcaea0e54aa65547c99c4418bf77ce84
ljs9904ljs <57557223+ljs9904ljs@users.noreply.github.com>
2024-11-25 23:17:14 +0900

    grpc 호출하는 API의 테스트를 추가

--- grpc-client/src/test/kotlin/com/example/grpcclient/GrpcClientApplicationTests.kt
+++ grpc-client/src/test/kotlin/com/example/grpcclient/GrpcClientApplicationTests.kt
@@ -1,40 +1,27 @@
 package com.example.grpcclient
 
-import com.example.api.student.grpc.StudentProto.StudentRequest
-import com.example.api.student.grpc.StudentServiceGrpcKt
-import io.grpc.ManagedChannelBuilder
+import com.example.grpcclient.api.student.StudentController
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.servlet.MockMvc
+
 
 @SpringBootTest
 class GrpcClientApplicationTests {
 
+    @Autowired
+    private lateinit var studentController: StudentController
 
     @Test
     fun getStudent() {
-        val channel = ManagedChannelBuilder
-            .forAddress("localhost", 7070)
-            .usePlaintext()  // default가 TLS 사용하는 건데, TLS 안 쓰게끔 세팅한다.
-            .build()
-
-        val stub = StudentServiceGrpcKt.StudentServiceCoroutineStub(channel)
-        val request = StudentRequest.newBuilder()
-            .setId(123)
-            .setEmail("asdf")
-            .setPassword("123123123")
-            .setName("asdfasdf")
-            .build()
 
         runBlocking {
-            val response = stub.getStudent(request)
-            println(response.id)
-            println(response.email)
-            println(response.password)
-            println(response.name)
-
+            val response = studentController.getStudent()
+            println(response)
         }
-
     }
 
 }
