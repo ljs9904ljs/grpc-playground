b32c5e55ef97169e342a1849925a2fb60d284d5a
ljs9904ljs <57557223+ljs9904ljs@users.noreply.github.com>
2024-11-25 23:17:34 +0900

    grpc-server에 걸맞게 설정 파일을 수정함.

--- grpc-server/build.gradle.kts
+++ grpc-server/build.gradle.kts
@@ -9,10 +9,11 @@ plugins {
 }
 
 group = "com.example"
-version = "0.0.1-SNAPSHOT"
+version = "1.0.0-SNAPSHOT"
 
 val protobufVersion = "4.28.3"
 val grpcVersion = "1.68.1"
+val grpcKotlinVersion = "1.4.1"
 
 java {
 	toolchain {
@@ -27,16 +28,22 @@ repositories {
 dependencies {
 	implementation("net.devh:grpc-server-spring-boot-starter:3.1.0.RELEASE")
 
-	// protobuf kotlin
+	// [protobuf] protobuf kotlin
 	implementation("com.google.protobuf:protobuf-kotlin:${protobufVersion}")
 
-	// protobuf java
+	// [protobuf] protobuf java
 	implementation("com.google.protobuf:protobuf-java:${protobufVersion}")
 	implementation("com.google.protobuf:protobuf-java-util:${protobufVersion}")
 
-	implementation("io.grpc:grpc-protobuf:${grpcVersion}")     // Protobuf 메시지와 gRPC의 통합을 지원
-	implementation("io.grpc:grpc-netty-shaded:${grpcVersion}") // Netty Shaded 사용(gRPC 서버와 클라이언트의 Netty 전송 계층을 제공)
-	implementation("io.grpc:grpc-stub:${grpcVersion}")         // gRPC 클라이언트 스텁을 생성
+	// [gRPC]
+	implementation("io.grpc:grpc-protobuf:${grpcVersion}")          // Protobuf 메시지와 gRPC의 통합을 지원
+	implementation("io.grpc:grpc-netty-shaded:${grpcVersion}")      // Netty Shaded 사용(gRPC 서버와 클라이언트의 Netty 전송 계층을 제공)
+	implementation("io.grpc:grpc-stub:${grpcVersion}")              // gRPC 클라이언트 스텁을 생성
+	implementation("io.grpc:grpc-kotlin-stub:${grpcKotlinVersion}") // Kotlin을 위해, gRPC 클라이언트 스텁을 생성
+
+	// grpc-kotlin 라이브러리가 coroutine을 사용하게 한다.
+	// https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
 
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -56,13 +63,26 @@ protobuf {
 		create("grpc") {
 			artifact = "io.grpc:protoc-gen-grpc-java:${grpcVersion}"
 		}
+
+		// '링크': https://github.com/grpc/grpc-kotlin/tree/master/compiler
+		// '링크'를 참고해서 작성했음.
+		create("grpc-kt") {
+			artifact = "io.grpc:protoc-gen-grpc-kotlin:1.4.1:jdk8@jar"
+		}
 	}
 	// 모든 프로토콜 버퍼 작업에 대해 gRPC 플러그인을 적용합니다.
 	generateProtoTasks {
 		all().forEach {
 			it.plugins {
 				create("grpc")
+				create("grpc-kt")
 			}
+
+			// 이거 작성하면 protoc로 kotlin_out 생성하는 것과 같은 결과물이 나온다.
+			// 그래서 쓸모가 없다.
+//            it.builtins {
+//                create("kotlin")
+//            }
 		}
 	}
 }
